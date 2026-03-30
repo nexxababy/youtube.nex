347b57c71ab89912e4bcf1504b953ad063866b65
Nexxababy <kartik18210@gmail.com>
2026-03-30 21:36:56 +0530

    Add files via upload

---
+++ youtubesearchpython/aio/__init__.py
@@ -0,0 +1,11 @@
+from youtubesearchpython.aio.search import Search, VideosSearch, ChannelsSearch, PlaylistsSearch, CustomSearch, ChannelSearch
+from youtubesearchpython.aio.extras import Video, Playlist, Suggestions, Hashtag, Comments, Transcript, Channel
+from youtubesearchpython.aio.streamurlfetcher import StreamURLFetcher
+from youtubesearchpython.core.utils import *
+from youtubesearchpython.core.constants import *
+
+
+__title__        = 'youtube-search-python'
+__version__      = '1.7.0'    
+__author__       = 'CertifiedCoders'
+__license__      = 'MIT'
